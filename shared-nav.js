3d6becd1b549ea71b377f0c15f3aacd29b98b23d
Sandeep Dwivedi <40598729+mail2sandeepd@users.noreply.github.com>
2026-08-19 19:23:53 +0530

    Fix formatting issues in shared-nav.js

--- shared-nav.js
+++ shared-nav.js
@@ -43,7 +43,7 @@ const sharedCalculators = [
   label: 'Lumpsum Calculator',
   href: 'lumpsum-calculator/',
   icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>`
- },
+  },
 ];
 
 function injectSharedSidebar(activeId, rootPath) {
